d9897062ba4b0484a6a597163e13982b8bdf67b3
Kunlin Yu <yukunlin@syriusrobotics.com>
2025-11-26 09:05:58 +0800

    Split Dbus2Http.h

--- include/dbus2http/Dbus2Http.h
+++ include/dbus2http/Dbus2Http.h
@@ -20,50 +20,14 @@ class Dbus2Http {
   std::thread service_thread_;
 
  public:
-  Dbus2Http(const std::vector<std::string>& service_prefixes, bool system)
-      : system_(system) {
-    service_prefixes_.insert(service_prefixes.begin(), service_prefixes.end());
-  }
+  Dbus2Http(const std::vector<std::string>& service_prefixes, bool system);
 
-  void start(int port) {
-    DbusEnumerator dbusEnumerator(context_);
-    auto service_names = DbusEnumerator::list_services();
-    for (const auto& service_name : service_names) {
-      if (not match_prefix(service_name)) continue;
-      PLOGI << "servcie name: " << service_name;
-      PLOGI << "object paths: ";
-      std::vector<ObjectPath> object_paths =
-          dbusEnumerator.parse_object_paths_recursively(service_name, "/");
-      object_paths.erase(
-          std::remove_if(
-              object_paths.begin(), object_paths.end(),
-              [](const ObjectPath& op) { return op.interfaces.empty(); }),
-          object_paths.end());
-      nlohmann::json j;
-      for (const auto& op : object_paths) j[op.path] = op;
-    }
+  void start(int port);
 
-    dbus_caller_ = std::make_unique<DbusCaller>(context_, system_);
-    service_ = std::make_unique<WebService>(*dbus_caller_);
-    service_thread_ = std::thread([&] { service_->run(port); });
-    PLOGI << "dbus2http started on port " << port << "...";
-  }
-
-  void stop() {
-    PLOGI << "stopping httplib service...";
-    service_->stop();
-    if (service_thread_.joinable()) service_thread_.join();
-    PLOGI << "httplib service stopped.";
-  }
+  void stop();
 
  private:
-  bool match_prefix(const std::string& service_name) {
-    if (service_prefixes_.empty()) return true;
-    for (const auto& prefix : service_prefixes_) {
-      if (service_name.rfind(prefix, 0) == 0) return true;
-    }
-    return false;
-  }
+  bool match_prefix(const std::string& service_name);
 };
 
 }  // namespace dbus2http
