9f6099078997f2fa6140110f0aa6b601b9903791
Kunlin Yu <yukunlin@syriusrobotics.com>
2025-12-01 15:08:03 +0800

    Use docker and cmake presets to build
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+conan install . --profile:host=docker/conan/profile_host_x86_64.txt
+cmake --preset x86_64-Release
+cmake --build --preset x86_64-Release
+
+conan install . --profile:host=docker/conan/profile_host_x86_64.txt --settings:host="build_type=Debug"
+cmake --preset x86_64-Debug
+cmake --build --preset x86_64-Debug
+
+conan install . --profile:host=docker/conan/profile_host_armv8.txt
+cmake --preset armv8-Release
+cmake --build --preset armv8-Release
